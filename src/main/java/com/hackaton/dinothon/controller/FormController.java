d361431f342a5f0d3e92746b6135a8a3fbc1b99c
Gerardo Carileo Acuña <gerardo.catrileo@gmail.com>
2022-08-25 00:03:52 -0400

    buscar

--- src/main/java/com/hackaton/dinothon/controller/FormController.java
+++ src/main/java/com/hackaton/dinothon/controller/FormController.java
@@ -3,18 +3,30 @@ package com.hackaton.dinothon.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.hackaton.dinothon.entity.Despacho;
 
 
 
 @Controller
-@SessionAttributes("usuario")
+@SessionAttributes("despacho")
 public class FormController {
 
 
+
 	@GetMapping("/")
-	public String form(Model model) {
-		return "index";
+	public String index(Model model) {
+		Despacho despacho = new Despacho();
+		model.addAttribute("despacho", despacho);
+		return "buscar";
+	}
+	
+	@PostMapping("/buscar")
+	public String buscar(Model model) {
+		
+		return "buscar";
 	}
 
 
